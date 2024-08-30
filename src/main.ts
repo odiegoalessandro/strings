31c547fac472b0e0a9d1fbe2d091bbf14c353d10
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2024-08-30 20:05:57 -0300

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
