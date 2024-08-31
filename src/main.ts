7986b2b88a96e6b3a96754a242ef60d548ba6175
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2024-08-30 22:12:23 -0300

    feat: configurando o swagger
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/main.ts
    	new file:   src/swagger/document-builder.config.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { PrismaClientExceptionFilter } from './filters/prisma-client-exception.filter';
+import { documentBuilder } from './swagger/document-builder.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,12 +14,15 @@ async function bootstrap() {
     new PrismaClientExceptionFilter(),
   );
 
-  app.use(
+  app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
