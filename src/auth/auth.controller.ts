c18bb21ac0fc6aabe7a96d2b6f1cf76494aee778
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2024-09-01 10:30:32 -0300

    fix: adicionando tag para as rotas auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
+@ApiTags('auth')
 @ApiResponse({ type: LoginResponseDto })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
